6e0e67503b720c2cdd1ab32ae75b45cc83e87efe
zecollokaris <collo.kariss@gmail.com>
2022-08-22 02:20:27 +0300

    created nav bar component section.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+  return(
+    <>
+    <nav className="navbar"> 
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">Cope</Link>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
