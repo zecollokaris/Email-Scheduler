a8081cfbf514b3cb70f93ee566f5d0884b1d97f8
zecollokaris <collo.kariss@gmail.com>
2022-08-23 01:35:19 +0300

    added phone image and started styling content

--- Frontend/src/components/EmailHome.js
+++ Frontend/src/components/EmailHome.js
@@ -40,18 +40,48 @@ class EmailHome extends React.Component {
   render() {
     return(
       <>
-      <nav className="navbar"> 
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">Cop</Link>
+
+      <div className="Email-Scheduler">
+        {/* Nav Section */}
+        <nav className="NavBar"> 
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+              <img src="/images/CopeMailerIcon2.png" className="Mailer-Icon" alt="CopeLogo" />
+            </Link>
+          </div>
+        </nav>
+
+
+        <div className="Home-Row">
+          <div className="Column-One">
+          <img src="/images/CopeIphone.png" className="CopePhone" alt="CopePhone" />
+          </div>
+          <div className="Column-Two" >
+
+            {/* Form Section */}
+            <div className="Form-Detail-Section">
+
+              {/* Title */}
+              <h1 className="Home-Title">Subscribe To Healthier Thoughts</h1>
+
+              {/* Enter Detail Here */}
+              <form onSubmit={this.handleSubmit}>
+                <p>Enter Your Email Adress Below</p>
+                  <input type="email" value={this.state.value} onChange={this.handleChange} />
+                
+                <button type="submit" value="Submit">Submit</button>
+              </form>
+
+              {/* SubTitle */}
+              <p className="Home-SubTitle">Your Daily Doze Of Mental Health Doctor</p>
+            </div>
+
+          </div>
         </div>
-        <form onSubmit={this.handleSubmit}>
-        <label>Enter Your Email:
-          <input type="email" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        
-        <button type="submit" value="Submit">Submit</button>
-      </form>
-      </nav>
+      </div>
+
+
+
       </>
     )
   }
